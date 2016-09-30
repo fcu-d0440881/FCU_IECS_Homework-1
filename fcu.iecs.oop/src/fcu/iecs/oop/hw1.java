797383edacf03a7d18b54416ee501c25f95a7431
Nameresu-PC\Nameresu <wu86rrexex@gmail.com>
2016-09-30 18:01:13 +0900

    刪減 開new string來存取

--- fcu.iecs.oop/src/fcu/iecs/oop/hw1.java
+++ fcu.iecs.oop/src/fcu/iecs/oop/hw1.java
@@ -3,9 +3,8 @@ package fcu.iecs.oop;
 public class hw1 {
 	public static void main(String[] args){
 		String lyrics = "Let it go! Let it go! Cannot hold it back anymore";
-		String new_lyrics = lyrics.replaceAll("it", "\"her\"");	
-		new_lyrics = new_lyrics.replaceAll("Cannot", "Can't");
-		lyrics = new_lyrics;
+		lyrics = lyrics.replaceAll("it", "\"her\"");	
+		lyrics = lyrics.replaceAll("Cannot", "Can't");
 		System.out.println("Launch : " + lyrics);
 	}
 }
