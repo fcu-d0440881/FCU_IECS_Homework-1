6d427533803f6a8c51b90c8c38d4acb7701aff60
fcu-d0440881 <nekosyndrome1997@gmail.com>
2016-09-30 12:34:34 +0900

    Launch lyrics by using string api

--- fcu.iecs.oop/src/fcu/iecs/oop/hw1.java
+++ fcu.iecs.oop/src/fcu/iecs/oop/hw1.java
@@ -3,5 +3,9 @@ package fcu.iecs.oop;
 public class hw1 {
 	public static void main(String[] args){
 		String lyrics = "Let it go! Let it go! Cannot hold it back anymore";
+		String new_lyrics = lyrics.replaceAll("it", "\"her\"");	
+		new_lyrics = new_lyrics.replaceAll("Cannot", "Can't");
+		lyrics = new_lyrics;
+		System.out.println("Launch : " + lyrics);
 	}
 }
